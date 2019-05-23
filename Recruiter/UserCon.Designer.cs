2a2fe509a1f9bc801899cc827c7ce262d2d8ea9e
Kuba <Kuba@KUBA-KOMPUTER>
2019-05-23 23:17:11 +0200

    UserCon added

--- Recruiter/UserCon.Designer.cs
+++ Recruiter/UserCon.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace Recruiter
 {
-    partial class Form1
+    partial class UserCon
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,39 +20,26 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.userCon1 = new Recruiter.UserCon();
             this.SuspendLayout();
             // 
-            // userCon1
-            // 
-            this.userCon1.Location = new System.Drawing.Point(12, 12);
-            this.userCon1.Name = "userCon1";
-            this.userCon1.Size = new System.Drawing.Size(882, 556);
-            this.userCon1.TabIndex = 0;
-            // 
-            // Form1
+            // UserCon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 580);
-            this.Controls.Add(this.userCon1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "UserCon";
+            this.Size = new System.Drawing.Size(608, 469);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private UserCon userCon1;
     }
 }
-
